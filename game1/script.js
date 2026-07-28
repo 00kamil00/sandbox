c112e09dd92b8b6ac86960d2462977e1edc47e33
00kamix00 <kamilkalemba6@gmail.com>
2026-07-28 23:53:55 +0200

    feat: handle buy and sell logic for all items using forEach

--- game1/script.js
+++ game1/script.js
@@ -1,44 +1,49 @@
 let mn = document.querySelector('#money')
-let sell = document.querySelector('.sell')
-let quantity = document.querySelector('.quantity')
-let buy = document.querySelector('.buy')
+let items = document.querySelectorAll('.item')
 
 let money = 750000000000
-let itemPrice = 2
-let itemQuantity = 0
 
 
-buy.addEventListener("click", () => {
-    if (money >= itemPrice) {
-    itemQuantity += 1
-    quantity.textContent = itemQuantity
-    
-    money -= itemPrice
-    mn.textContent = '$' + money.toLocaleString('en-US')
-    }
+items.forEach((item) => {
+    let sell = item.querySelector('.sell')
+    let quantity = item.querySelector('.quantity')
+    let buy = item.querySelector('.buy')
+    let price = item.querySelector('.price')
 
-    if (itemQuantity > 0) {
-        sell.classList.add('can-sell')
-    } else {
-    sell.classList.remove('can-sell')
-    }
+    let itemQuantity = 0
+    let itemPrice = Number(price.textContent.replace(/[^0-9]/g, ''))
+
+
+    buy.addEventListener("click", () => {
+        if (money >= itemPrice) {
+            itemQuantity += 1
+            quantity.textContent = itemQuantity
+
+            money -= itemPrice
+            mn.textContent = '$' + money.toLocaleString('en-US')
+        }
+
+        if (itemQuantity > 0) {
+            sell.classList.add('can-sell')
+        } else {
+            sell.classList.remove('can-sell')
+        }
+    })
+
+
+    sell.addEventListener("click", () => {
+        if (itemQuantity > 0) {
+            itemQuantity -= 1
+            quantity.textContent = itemQuantity
+
+            money += itemPrice
+            mn.textContent = '$' + money.toLocaleString('en-US')
+        }
+
+        if (itemQuantity > 0) {
+            sell.classList.add('can-sell')
+        } else {
+            sell.classList.remove('can-sell')
+        }
+    })
 })
-
-
-sell.addEventListener("click", () => {
-    if (itemQuantity > 0) {
-        itemQuantity -= 1
-        quantity.textContent = itemQuantity
-
-        money += itemPrice
-        mn.textContent =  '$' + money.toLocaleString('en-US')
-    }
-
-    if (itemQuantity > 0) {
-        sell.classList.add('can-sell')
-    } else {
-        sell.classList.remove('can-sell')
-    }
-})
-
-
