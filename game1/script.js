5009fce24bb6b27cb76a7801ca8081134a4fc340
00kamix00 <kamilkalemba6@gmail.com>
2026-07-30 13:32:26 +0200

    feat: rename header to <kamil.dev/> and add dynamic receipt section

--- game1/script.js
+++ game1/script.js
@@ -1,14 +1,18 @@
-let mn = document.querySelector('#money')
-let items = document.querySelectorAll('.item')
+const mn = document.querySelector('#money')
+const items = document.querySelectorAll('.item')
+const moneyBar = document.querySelector('#money-bar')
+const receiptItems = document.querySelector('#receipt-items')
+const totalPrice = document.querySelector('#total-price')
 
 let money = 750000000000
+let start_money = 750000000000
 
 
 items.forEach((item) => {
-    let sell = item.querySelector('.sell')
-    let quantity = item.querySelector('.quantity')
-    let buy = item.querySelector('.buy')
-    let price = item.querySelector('.price')
+    const sell = item.querySelector('.sell')
+    const quantity = item.querySelector('.quantity')
+    const buy = item.querySelector('.buy')
+    const price = item.querySelector('.price')
 
     let itemQuantity = 0
     let itemPrice = Number(price.textContent.replace(/[^0-9]/g, ''))
@@ -24,6 +28,7 @@ items.forEach((item) => {
 
             updateMoneyBar()
             updateAllBuyButtons()
+            updateReceipt()
         }
 
         if (itemQuantity > 0) {
@@ -44,6 +49,7 @@ items.forEach((item) => {
 
             updateMoneyBar()
             updateAllBuyButtons()
+            updateReceipt()
         }
 
         if (itemQuantity > 0) {
@@ -56,15 +62,12 @@ items.forEach((item) => {
 
 
 
-let start_money = 750000000000
-const moneyBar = document.querySelector('#money-bar')
 
 function updateMoneyBar() {
     let left_money_percent = (money / start_money) * 100
 
     moneyBar.style.width = left_money_percent + '%'
 }
-
 
 
 
@@ -84,3 +87,32 @@ function updateAllBuyButtons() {
 }
 
 updateAllBuyButtons()
+
+
+
+
+function updateReceipt() {
+    receiptItems.innerHTML = ""
+    let totalSpent = 0
+
+    items.forEach((item) => {
+        const itemName = item.querySelector('.item-name').textContent
+        const price = item.querySelector('.price')
+        const quantity = item.querySelector('.quantity')
+        let itemPrice = Number(price.textContent.replace(/[^0-9]/g, ''))
+        let itemQuantity = Number(quantity.textContent)
+
+        if (itemQuantity > 0) {
+            let cost = itemQuantity * itemPrice
+            totalSpent += cost
+
+            receiptItems.innerHTML += `
+                <div class="receipt-row">
+                    <p>${itemName} x ${itemQuantity}</p>
+                    <p>$${cost.toLocaleString('en-US')}</p>
+                </div>
+            `
+        }
+    })
+    totalPrice.textContent = "$" + totalSpent.toLocaleString('en-US')
+}
